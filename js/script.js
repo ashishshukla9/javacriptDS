20c2249b2c5cdb3b8721d727e255d12ca388ba10
Ashish Shukla <shuklaashish655@gmail.com>
2024-05-02 16:32:11 +0530

    Day -2

--- js/script.js
+++ js/script.js
@@ -1,14 +1,21 @@
-// const findLongestWord = (str) => {
-//  strArr = str.split("")
-//  console.log(strArr)
+// Day -1
+
+// function findLongestWord(str) {
+//     words = str.split(" ");
+//     words = words.sort((a,b) =>b.length-a.length)
+//     console.log(words);
+//     return words[0];
 // }
+// console.log(findLongestWord("My Name is Ashish Shukla and i am a frontend developer")) 
 
-// findLongestWord("My Name is Ashish Shukla and i am a frontend developer")
+// Day - 2
 
-function findLongestWord(str) {
-    words = str.split(" ");
-    words = words.sort((a,b) =>b.length-a.length)
-    console.log(words);
-    return words[0];
+const generateHash = (str) => {
+    text = str.split(" ")
+    //    console.log(text)
+    text = text.map((curElem) => curElem.replace(curElem[0], curElem[0].toUpperCase()));
+    text = `#${text.join("")}`;
+    // console.log(text)
+    return text
 }
-console.log(findLongestWord("My Name is Ashish Shukla and i am a frontend developer")) 
+console.log(generateHash("ashish shukla"));
--- readme.md
+++ readme.md
@@ -1,3 +1,6 @@
 Day - 1 Write a function findLongestWord that takes a string as input and  returns the  longest word in thie string. if there are multiple  longest words,
 
-using split and sort function for find longest word with compare use in this programm
+<!-- using split and sort function for find longest word with compare use in this programm -->
+
+Day - 2 Create to implementing a function generateHash that generates a hash tag from a given input string.
+
