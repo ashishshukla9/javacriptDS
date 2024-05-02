817574d5d62905e203ad996c515741486e19f8cc
Ashish Shukla <shuklaashish655@gmail.com>
2024-05-02 17:01:08 +0530

    Day - 3

--- js/script.js
+++ js/script.js
@@ -10,12 +10,28 @@
 
 // Day - 2
 
-const generateHash = (str) => {
-    text = str.split(" ")
-    //    console.log(text)
-    text = text.map((curElem) => curElem.replace(curElem[0], curElem[0].toUpperCase()));
-    text = `#${text.join("")}`;
-    // console.log(text)
-    return text
+// const generateHash = (str) => {
+//     text = str.split(" ")
+//     //    console.log(text)
+//     text = text.map((curElem) => curElem.replace(curElem[0], curElem[0].toUpperCase()));
+//     text = `#${text.join("")}`;
+//     // console.log(text)
+//     return text
+// }
+// console.log(generateHash("ashish shukla"));
+
+// Day -3
+
+const findTotal = (word, char) => {
+    word = word.toLowerCase();
+    char = char.toLowerCase();
+    totalCount = word.split("").reduce((accum, curChar) => {
+        if (curChar === char) {
+            accum++;
+        }
+        return accum;
+    },0)
+   
+    return totalCount;
 }
-console.log(generateHash("ashish shukla"));
+console.log(findTotal("MissIssippi", "I"))
